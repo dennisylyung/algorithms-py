04bb00e018b309e6deb16014ebc174ee67bd833e
Dennis Yung <dennisylyung@gmail.com>
2021-10-20 12:52:56 +0100

    b+ tree slicing

--- b_plus_tree.py
+++ b_plus_tree.py
@@ -23,7 +23,12 @@ class BPlusTree(Generic[K, V]):
         self.root = self.root.get_root()
 
     def __getitem__(self, key: K) -> V:
-        return self.root.search(key)
+        if isinstance(key, slice):
+            if key.step:
+                raise NotImplementedError('step in key slicing not supported')
+            return self.root.get_range(key.start, key.stop)
+        elif isinstance(key, int):
+            return self.root.search(key)
 
     def __len__(self) -> int:
         return len(self.root)
@@ -73,6 +78,32 @@ class _BPlusTreeImpl(Generic[K, V]):
             else:
                 raise KeyError
 
+    def get_range(self, start_key: Union[K, None], end_key: K) -> List[V]:
+        if start_key is not None:
+            i = self._index(start_key)
+        else:
+            i = 0
+        if self.is_leaf:
+            results = []
+            if i % 2:  # odd index means an exact match
+                target = self.children[i - 1]
+                if type(target) is not _BPlusTreeImpl:
+                    results.append(target)
+                i += 1
+            while self.children[i] is not None:
+                target = self.children[i]
+                if type(target) is _BPlusTreeImpl:
+                    results += target.get_range(None, end_key)
+                    break
+                else:
+                    if end_key < self.children[i + 1]:
+                        break
+                    results.append(target)
+                i += 2
+            return results
+        else:
+            return self.children[i + 1 if i % 2 else i].get_range(start_key, end_key)
+
     def insert(self, key: K, value: V) -> None:
         i = self._index(key)
 
@@ -80,12 +111,15 @@ class _BPlusTreeImpl(Generic[K, V]):
             self.children[i - 1] = value  # replace value: this implementation does not support duplicate keys
         else:
             if self.is_leaf or type(value) is _BPlusTreeImpl:
+                if self.is_leaf:
+                    resulting_children = self.children[:i] + [value, key] + self.children[i:]
+                else:  # type(value) is _BPlusTreeImpl
+                    resulting_children = self.children[:i + 1] + [key, value] + self.children[i + 1:]
                 if self.is_full:
-                    self._split_and_insert(key, value, i)
+                    self._split_and_insert(resulting_children)
                 else:
-                    data_after = self.children[i:]
-                    self.children[i + 2:i + len(data_after)] = data_after[:-2]
-                    self.children[i:i + 2] = [value, key]
+                    self.children = resulting_children[:-2]
+
             else:
                 self.children[i + 1 if i % 2 else i].insert(key, value)
 
@@ -113,23 +147,34 @@ class _BPlusTreeImpl(Generic[K, V]):
                 i += 2
         pass
 
-    def _split_and_insert(self, key: K, value: V, i: int) -> None:
-        temp_nodes = self.children[:i] + [value, key] + self.children[i:]
+    def _split_and_insert(self, resulting_children: List[
+        Union[None, K, V, _BPlusTreeImpl[K, V]]]) -> None:
         mid_point = ceil((self.node_len + 2) / 2)
+        new_node = _BPlusTreeImpl(self.b, self.parent, resulting_children[mid_point:])
         if self.is_leaf:
-            split_value = temp_nodes[mid_point + 1]
-            new_node = _BPlusTreeImpl(self.b, None, temp_nodes[:mid_point] + [self])
+            split_value = resulting_children[mid_point + 1]
+            self.children = [None] * self.node_len
+            self.children[:mid_point + 1] = resulting_children[:mid_point] + [new_node]
         else:
-            split_value = temp_nodes[mid_point - 1]
-            new_node = _BPlusTreeImpl(self.b, None, temp_nodes[:mid_point - 1])
-        self.children = [None] * self.node_len
-        self.children[:len(temp_nodes) - mid_point] = temp_nodes[mid_point:]
+            split_value = resulting_children[mid_point - 1]
+            self.children = [None] * self.node_len
+            self.children[:mid_point - 1] = resulting_children[:mid_point - 1]
+        new_node._sync_children()
+        self._sync_children()
         if self.parent is not None:
             self.parent.insert(split_value, new_node)
         else:
-            parent = _BPlusTreeImpl(self.b, None, [new_node, split_value, self])
+            parent = _BPlusTreeImpl(self.b, None, [self, split_value, new_node])
             new_node.parent = parent
             self.parent = parent
+            pass
+
+    def _sync_children(self) -> None:
+        if self.is_leaf:
+            return
+        for node in self.children:
+            if type(node) is _BPlusTreeImpl:
+                node.parent = self
 
 
 class BPlusTreeImplTest(TestCase):
@@ -143,10 +188,10 @@ class BPlusTreeImplTest(TestCase):
         self.tree.children = children
 
     def test_index(self):
-        self.assertEqual(0, self.tree.index(-10))
-        self.assertEqual(1, self.tree.index(1))
-        self.assertEqual(4, self.tree.index(2.5))
-        self.assertEqual(8, self.tree.index(10))
+        self.assertEqual(0, self.tree._index(-10))
+        self.assertEqual(1, self.tree._index(1))
+        self.assertEqual(4, self.tree._index(2.5))
+        self.assertEqual(8, self.tree._index(10))
 
     def test_search_single_layer(self):
         self.assertEqual('data_1', self.tree.search(1))
@@ -175,6 +220,10 @@ class BPlusTreeUITest(TestCase):
     def test_get(self):
         for i in range(50, 7):
             self.assertEqual(f'data_{i}', self.tree[i])
+
+    def test_slice(self):
+        expected = ['data_13', 'data_14', 'data_15', 'data_16', 'data_17', 'data_18', 'data_19']
+        self.assertEqual(expected, self.tree[12.5:19])
 
     def test_insert(self):
         self.tree[100] = 'data_100'
